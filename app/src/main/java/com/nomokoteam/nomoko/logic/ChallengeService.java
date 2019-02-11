1eebb6b5a1e4a2cee93db4de17b9a5de6226be81
Timon <timon.demorel@hotmail.fr>
2019-02-11 15:54:00 +0100

    Création de ChallengerService

---
+++ app/src/main/java/com/nomokoteam/nomoko/logic/ChallengeService.java
@@ -0,0 +1,15 @@
+package com.nomokoteam.nomoko.logic;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ChallengeService extends IntentService {
+    public ChallengeService () {
+        super("ChallengeService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
