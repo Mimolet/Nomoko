7cdc1ca486914c1b1b4876d6416dae098652e917
Timon De Morel <timon.demorel@hotmail.fr>
2019-02-20 16:00:33 +0100

    Ajout des premiers designs de vue

---
+++ app/src/main/java/com/n/nomoko/views/RecompenseActivity.java
@@ -0,0 +1,14 @@
+package com.n.nomoko.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.n.nomoko.*;
+
+public class RecompenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recompense);
+    }
+}
